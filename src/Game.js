75feb9711344f55cb8b36c3280bcd3f463678913
AleksiDu <aleksi.duluzauri@gmail.com>
2023-02-09 17:27:16 +0400

    setup all necessary componets

---
+++ src/Game.js
@@ -0,0 +1,12 @@
+import "./Game.css";
+import Field from "./components/Field";
+
+function Game() {
+  return (
+    <div className="Game">
+      <Field />
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/components/CreateField.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateField = () => {
+  return <div>CreateField</div>;
+};
+
+export default CreateField;
